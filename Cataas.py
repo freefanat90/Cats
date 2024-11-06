9db2acf9af993fe9e64134a3388f1e9e41531fa6
freefanat90 <freefanat90@gmail.com>
2024-11-07 00:31:26 +0300

    Добавили кнопку для загрузки нового изображения

--- Cataas.py
+++ Cataas.py
@@ -3,16 +3,22 @@ import requests
 from PIL import Image, ImageTk
 from io import BytesIO
 
-def loade_image():
+def load_image(url):
     try:
         response = requests.get(url)
-        response.rise_for_status()
+        response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f'Произошла ошибка: {e}')
         return None
+
+def set_image():
+    img = load_image(url)
+    if img:
+        label.config(image=img)
+        label.image = img
 
 window = Tk()
 window.title('Cats!')
@@ -21,11 +27,11 @@ window.geometry('600x400')
 label = Label()
 label.pack()
 
-url = 'https://cataas.com/cat'
-img = load_image(url)
+update_button = Button(text='Обновить', command=set_image)
+update_button.pack()
 
-if img:
-    label.config(image=img)
-    label.image = img
+url = 'https://cataas.com/cat'
+
+set_image()
 
 window.mainloop()
