b37e611df51114b4a8abd0630fced5925b596719
freefanat90 <freefanat90@gmail.com>
2024-11-06 18:40:43 +0300

    Создано окно и виджеты

--- Cataas.py
+++ Cataas.py
@@ -1,1 +1,23 @@
+from cProfile import label
 from tkinter import *
+from tkinter import Label
+
+import requests
+from PIL import Image, ImageTk
+from io import BytesIO
+
+window = Tk()
+window.title('Cats!')
+window.geometry('600x400')
+
+label = Label()
+label.pack
+
+url = 'https://cataas.com/cat'
+img = load.image(url)
+
+if img:
+    label.config(image=img)
+    label.image = img
+
+window.mainloop()
